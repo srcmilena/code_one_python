b640882d6f1fb0913709e55a52d1b03fcef872fe
srcmilena <87768754+srcmilena@users.noreply.github.com>
2022-05-14 20:48:58 -0300

    first commit

---
+++ c02_data_input_output.py
@@ -0,0 +1,4 @@
+print('hello, world') #python accepts "" or '' | print = output
+print('welcome to the pyhton course by coding tank')
+
+input('what is the programming language that you are studying?') #input data
