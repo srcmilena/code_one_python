b15db75394498efa1a15fdf5827e31318c6da351
srcmilena <87768754+srcmilena@users.noreply.github.com>
2022-05-15 09:33:24 -0300

    adding exercises

---
+++ c03_variables.py
@@ -0,0 +1,44 @@
+"""
+this is a comment
+this is also a comment
+"""
+
+# creating variables
+age = 26 # age "receives" (not equal to) 26
+# what is on the right side is attributed to what is on the left side
+
+print(age)
+
+name = "milena"
+
+print(name)
+
+"""
+Variables types
+1. int: integers numbers, without decimals: 0, 5, -1, 1000
+2. float: "real" numbers, with decimals: 1.0, -2.7, 3.14 
+3. str: characters, texts (string)
+4. bool: logical values, true or false
+"""
+
+age = 5
+height = 1.70
+name = "milena"
+studying = True
+
+# python is caseSentive
+
+#to identify a variable type
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(studying))
+
+# getiing data from the user and saving in a variable
+language = inpout("what is the programming language you are learning?") # created a variable "language" and what is being typed will be catched to language
+
+print("the language you are studying is ", language)
+
+# cocatianation = period
+
+#cls = cleans terminal
